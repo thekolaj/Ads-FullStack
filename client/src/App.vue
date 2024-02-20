680f9e1a42abe9fd4515493d606cbeacd281ffae
kolaj <thekolaj@gmail.com>
2024-02-20 22:32:19 +0200

    Front end base

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { useDark, useToggle } from '@vueuse/core'
+import { FwbButton } from 'flowbite-vue'
+
+const isDark = useDark({ disableTransition: false })
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <RouterView />
+  <header>
+    <FwbButton size="sm" pill @click="toggleDark()">
+      {{ isDark ? 'Dark' : 'Light' }}
+    </FwbButton>
+  </header>
+  <main>
+    <RouterView />
+  </main>
 </template>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,5 @@
 import './assets/style.css'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
