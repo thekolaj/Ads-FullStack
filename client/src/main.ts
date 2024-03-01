e49d95bff69ffa5e279870d709e9f7fda6871ef2
kolaj <thekolaj@gmail.com>
2024-03-01 20:04:28 +0200

    fix: deployment tracking

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import * as Sentry from '@sentry/vue'
-import { apiOrigin, sentryDSN } from '@/config'
+import { sentryDSN } from '@/config'
 
 import App from './App.vue'
 import router from './router'
@@ -23,8 +23,6 @@ if (sentryDSN) {
     ],
     // Performance Monitoring
     tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    tracePropagationTargets: ['localhost', apiOrigin],
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
